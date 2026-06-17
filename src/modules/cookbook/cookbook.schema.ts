789d81e2f26b0049561fa833cb87cbeccbb06807
Ankur Mondal <mondalankur2007@gmail.com>
2026-06-18 01:23:09 +0530

    configured the server for vercel deployment

--- src/modules/cookbook/cookbook.schema.ts
+++ src/modules/cookbook/cookbook.schema.ts
@@ -3,7 +3,7 @@ export const AuthSchema = z.object({
   body: z.object({
     username: z
       .string({ message: "Username is required" })
-      .min(3, "Username must be at least 8 characters long")
+      .min(3, "Username must be at least 3 characters long")
       .max(30, "Username cannot exceed 30 characters")
       .regex(
         /^[a-zA-Z0-9_]+$/,
