20a16aadfd814c38a9e82c0327f5490d3289b4f8
Ankur Mondal <mondalankur2007@gmail.com>
2026-07-10 14:36:39 +0530

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { connectDB } from './config/database';
-import { connectCloudinary } from './config/cloudinary';
-import masterRouter from './routes';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/database";
+import { connectCloudinary } from "./config/cloudinary";
+import masterRouter from "./routes";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://cookbookguide.dpdns.org"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Mount routes
-app.use('/api/v1', masterRouter);
+app.use("/api/v1", masterRouter);
 
 // Initialize external services
 connectCloudinary();
-connectDB(); 
+connectDB();
 
 export default app; // IMPORTANT: We export the app instead of listening
